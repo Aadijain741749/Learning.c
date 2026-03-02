182829a62b7dfa31ff7ecab5835008303b16309b
Aadi Jain <aadi.25gcebcs045@galgotiacollege.edu>
2026-03-02 15:16:07 +0530

    Add files via upload

---
+++ escape.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+  printf("Aadi\nJain\n");
+  printf("Aadi\tJain\n");
+  printf("Aadi \bJain\n");
+  printf("Aadi\"Jain\"");
+  return 0;
+}
---
+++ variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define Age = 17;
+int main()
+{
+  const float pi = 3.14;
+
+  int a = 5;
+
+  float out;
+
+  printf("%d\n",a);
+
+  a = 6;
+  printf("%d",a);
+
+  out = 4.4;
+
+  return 0;
+}
